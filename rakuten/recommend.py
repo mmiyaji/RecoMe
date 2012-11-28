4379faf3fc1c2b909990e6222cc1ade5710d7e5f
mmiyaji <mmiyaji@ruhenheim.org>
2012-11-29 01:18:28 +0900

    bug fix

--- rakuten/recommend.py
+++ rakuten/recommend.py
@@ -18,13 +18,13 @@ class Recom():
             g,self.G = cache.get(memkey)
         else:
             try:
-                self.G=pickle.load(open(os.path.join(settings.BASE_DIR,'nx.dump')))
+                self.G=pickle.load(open(os.path.join(settings.TMP_DIR,'nx.dump')))
             except:
                 g,self.G = self.recalc_network(memkey)
                 pickle.dump(self.G,open(os.path.join(settings.TMP_DIR,'nx.dump'),'w'))
-        for c,i in enumerate(self.G.nodes()):
-            self.get_path(i)
-            print c,i
+#        for c,i in enumerate(self.G.nodes()):
+#            self.get_path(i)
+#            print c,i
     def get_network(self):
         """
         Arguments:
@@ -34,14 +34,15 @@ class Recom():
     def get_path(self, w1, w2 = ""):
         p = "netpath_%s" % w1
         if p in cache:
+            print 'f',p
             return cache.get(p)
         else:
             length,path=nx.single_source_dijkstra(self.G, w1)
-            print cache.set(p, [length,path], 0)
+            print cache.set(p, [length,path], 300000)
             if p in cache:
-                print 'found'
+                print 'found',p
             else:
-                print 'not found'
+                print 'not found',p
         if w2:
             return length[w2],path[w2]
         else:
